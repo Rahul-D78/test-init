ff4d531697889b697b1b58d9a0454d944965e3c9
Rahul-D78 <rahulparida933@gmail.com>
2021-08-01 14:43:34 +0530

    :fire: bootstrap ttd using stacks
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

---
+++ README.md
@@ -0,0 +1,42 @@
+## Project setup from scratch :
+
+* Initiate your project with vitejs :
+```$ npm init @vitejs/app```
+
+* Select the vanila javascript option : 
+
+* Install `Jest` :
+```$ npm i jest --save-dev```
+
+* First failing test :
+```$ npm run test```
+
+* For type safety :
+```
+npm i @types/jest --save-dev
+
+create a jsconfig.json file in your root directory
+
+{
+  "typeAcquistion": {
+      "include": [
+         "jest"
+      ]
+   }
+}
+```
+
+```
+1. we are using stack data structure to implemt test cases
+2. create a test directory and inside that 
+3. create a stack.test.js
+4. and write all your test cases inside it 
+```
+
+## direct clone from github :
+
+* Install the dependencies
+``` npm install ```
+
+* Run the app
+``` npm start ```
---
+++ test/sample.test.js
@@ -0,0 +1,53 @@
+class Stack {
+    constructor() {
+        this.top = -1;
+        this.items = {};
+    }
+    get peek() {
+        return this.items[this.top];
+    }
+    get empty() {
+        return this.top === -1;
+    }
+    push(value) {
+        this.top += 1;
+        this.items[this.top] = value;
+    }
+    pop() {
+        if (!this.empty) {
+            let value = this.peek;
+            this.top -= 1;
+            this.items[this.top] = value;
+        }
+    }
+}
+
+describe('My stack', () => {
+
+    let stack;
+
+    beforeEach(() => {
+        stack = new Stack();
+    });
+
+    it('is created empty', () => {
+        expect(stack.top).toBe(-1);
+        expect(stack.items).toEqual({});
+    })
+
+    it('can push to the top', () => {
+        stack.push(1);
+        expect(stack.top).toBe(0);
+        expect(stack.peek).toBe(1);
+    });
+
+    it('can pop off', () => {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        expect(stack.top).toBe(-1);
+    });
+})
